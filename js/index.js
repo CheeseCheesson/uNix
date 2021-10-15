56d3f6055a12d865cac9fc5be93d657869f2283f
CheeseCheesson <kira7999@gmail.com>
2021-10-15 17:30:49 +0300

    add second dropdown

--- js/index.js
+++ js/index.js
@@ -3,7 +3,9 @@ const subNav = document.querySelector(".sub-nav__items");
 const auth = document.querySelector("#auth");
 const popup = document.querySelector(".popup");
 const popUpArea = document.querySelector(".popup__area");
-const rules = document.querySelector('#rules')
+const rules = document.querySelector("#rules");
+const rule = document.querySelector(".rule");
+
 function show() {
   subNav.classList.toggle("_active");
 }
@@ -16,13 +18,18 @@ function showAuth() {
 function authRemove() {
   popup.classList.remove("_active");
 }
-
+function showRules() {
+  rule.classList.toggle("_active");
+}
+function removeRules() {
+  rule.classList.remove("_active");
+}
 shop.addEventListener("mouseover", show);
-
 subNav.addEventListener("mouseleave", hide);
-
 auth.addEventListener("click", showAuth);
-
 popUpArea.addEventListener("click", authRemove);
-
+rules.addEventListener("mouseover", hide);
+rules.addEventListener("mouseover", showRules);
+rule.addEventListener("mouseleave", removeRules);
+shop.addEventListener("mouseover", removeRules)
 
